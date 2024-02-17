68d22cd8321c1a40a0a47686828b9a4b97fc5153
mbilal71 <mbilal71@gmail.com>
2024-02-17 15:11:46 +0500

    First Commit

---
+++ index.js
@@ -0,0 +1,55 @@
+"use strict";
+/*
+Store a person’s name in a variable, and print a message to that person. Your message should
+be simple, such as, “Hello Eric, would you like to learn some Python today?”
+*/
+// // Print 
+// var personName : string = "eric";
+// // Print 
+// console.log("Hello "+ personName + ",Would you like to learn PYTHON today?");
+//////////////////////////////////////////////////////////////////
+/*
+Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase.
+*/
+// var personName: string = "eric";
+// // lower case
+// console.log(personName.toLowerCase());
+// // UPPER CASE
+// console.log(personName.toUpperCase());
+//////////////////////////////////////////////////////////////////
+/*
+Find a quote from a famous person you admire. Print the quote and the name of its author.
+Your output should look something like the following, including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+*/
+// // famous person
+// var famousPerson : string = "Albert Einstein";
+// // Print
+// console.log(famousPerson + " once said,“A person who never made a mistake never tried anything new.”");
+//////////////////////////////////////////////////////////////////
+/*
+Repeat Exercise 4, but this time store the famous person’s name in a variable called
+famous_person. Then compose your message and store it in a new variable called message. Print your
+message.
+*/
+// // famous person
+// var famous_person: string = "Albert Einstein";
+// // Quote
+// var message: string = "“A person who never made a mistake never tried anything new.”";
+// // Print
+// console.log(famous_person + " once said," + message);
+//////////////////////////////////////////////////////////////////
+/*
+Store a person’s name, and include some whitespace characters at the beginning and end of
+the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name
+*/
+// Normal name
+var normalName = "Hassan";
+// Using "\t"
+var name1 = "\tHassan";
+// Using "\n"
+var name2 = "My name is \nHassan";
+// Print
+console.log(normalName + "<= Normal");
+console.log(name1 + "<= Using \t");
+console.log(name2);
